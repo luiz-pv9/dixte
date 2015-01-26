a05abcbcce881b0a8eb0dcaf6c555d659c06b40d
Luiz Paulo de Vasconcellos <luiz.pv9@gmail.com>
2015-01-26 17:19:59 -0200

    First commit. Developing jsonvalidation

---
+++ jsonvalidation/jsonvalidation.go
@@ -0,0 +1,34 @@
+package jsonvalidation
+
+type JsonValidator func(val interface{}) bool
+
+// Returns true if the specified argument is of the type string
+func AnyString(val interface{}) bool {
+	_, ok := val.(string)
+	return ok
+}
+
+// The AnyNumber JsonValidator only checks for numeric values in the format
+// of float64 because it's the format go native json will unmarshal any number.
+func AnyNumber(val interface{}) bool {
+	_, ok := val.(float64)
+	return ok
+}
+
+// Returns true if the specified argument is of the type boolean (true or false)
+func AnyBoolean(val interface{}) bool {
+	_, ok := val.(bool)
+	return ok
+}
+
+// Generates a function that matches the JsonValidator type to compare string
+// against the specified value. Usually will be used for keys in a JSON object.
+func ExactString(str string) JsonValidator {
+	return func(val interface{}) bool {
+		stringVal, ok := val.(string)
+		if ok == false {
+			return false
+		}
+		return str == stringVal
+	}
+}
---
+++ jsonvalidation/jsonvalidation_test.go
@@ -0,0 +1,90 @@
+package jsonvalidation
+
+import "testing"
+
+func TestAnyString(t *testing.T) {
+	var fn JsonValidator
+	fn = AnyString
+	if fn("foobar") != true {
+		t.Error("Didn't match foobar string")
+	}
+
+	if fn("") != true {
+		t.Error("Didn't match empty string")
+	}
+
+	if fn(nil) != false {
+		t.Error("Matched nil argument")
+	}
+
+	if fn(10) != false {
+		t.Error("Matched number argument")
+	}
+}
+
+func TestAnyNumber(t *testing.T) {
+	var fn JsonValidator
+	fn = AnyNumber
+
+	if fn(float64(30)) != true {
+		t.Error("Didn't match 30")
+	}
+
+	if fn(float64(31.12312312)) != true {
+		t.Error("Didn't match with decimal points")
+	}
+
+	if fn(nil) != false {
+		t.Error("Matched a nil value")
+	}
+
+	if fn("123") != false {
+		t.Error("Matched a string value")
+	}
+}
+
+func TestAnyBoolean(t *testing.T) {
+	var fn JsonValidator
+	fn = AnyBoolean
+
+	if fn(false) != true {
+		t.Error("Didn't match false value")
+	}
+
+	if fn(true) != true {
+		t.Error("Didn't match true value")
+	}
+
+	if fn(1) != false {
+		t.Error("Matched number value as boolean")
+	}
+
+	if fn(nil) != false {
+		t.Error("Matched a nil value")
+	}
+}
+
+func TestExactString(t *testing.T) {
+	var fn JsonValidator
+	fn = ExactString("luiz")
+	if fn("luiz") != true {
+		t.Error("Didn't match exact string (luiz)")
+	}
+
+	if fn("Luiz") != false {
+		t.Error("Matched string with different case (Luiz)")
+	}
+
+	if fn("") != false {
+		t.Error("Matched empty string")
+	}
+
+	if fn(nil) != false {
+		t.Error("Matched nil argument")
+	}
+
+	fn = ExactString("123")
+	if fn(123) != false {
+		t.Error("Matched number argument although equal('123', 123)")
+	}
+}
